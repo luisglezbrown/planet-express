77fa5583aa91cb46927297c6e8f696f8102ef60a
luisglezbrown <lfernanglez@gmail.com>
2021-09-03 21:53:35 +0200

    Created Countdown component

---
+++ src/components/OfferCountdown.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from 'react';
+import { COUNTDOWN_INITIAL_MINUTES, COUNTDOWN_INITIAL_SECONDS } from '../config/config';
+
+
+export default function OfferCountdown () {
+
+    const [minutesRemaining, setMinutesRemaining] = useState(COUNTDOWN_INITIAL_MINUTES);
+    const [secondsRemaining, setSecondsRemaining] = useState(COUNTDOWN_INITIAL_SECONDS);
+    
+    useEffect(()=>{
+        let offerInterval = setInterval(() => {
+
+            if (secondsRemaining > 0) {
+                setSecondsRemaining(secondsRemaining - 1);
+            }
+
+            if (secondsRemaining === 0) {
+                if (minutesRemaining === 0) {
+                    clearInterval(offerInterval)
+                } else {
+                    setMinutesRemaining(minutesRemaining - 1);
+                    setSecondsRemaining(59);
+                }
+            } 
+
+        }, 1000);
+
+        return ()=> {
+            clearInterval(offerInterval);
+        };
+    });
+
+    return (
+        <>
+            { minutesRemaining === 0 && secondsRemaining === 0
+                ? null
+                : <h1> La oferta expira en {minutesRemaining}:{secondsRemaining < 10 ?  `0${secondsRemaining}` : secondsRemaining}</h1> 
+            }
+        </>
+    )
+}
+
