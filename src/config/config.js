a7472473da189efa0195d1d84dd8463893ac1ace
luisglezbrown <lfernanglez@gmail.com>
2021-09-03 21:55:04 +0200

    Added countdown params

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,14 @@
+// Set the price calculator params
 const PACKAGE_FEE = 1.95;
 const PRICE_PER_KG = 0.95;
 
+// Set the initial countdown duration
+const COUNTDOWN_INITIAL_MINUTES = 3;
+const COUNTDOWN_INITIAL_SECONDS = 0;
+
 export {
     PACKAGE_FEE,
-    PRICE_PER_KG    
+    PRICE_PER_KG,
+    COUNTDOWN_INITIAL_MINUTES, 
+    COUNTDOWN_INITIAL_SECONDS    
 };
