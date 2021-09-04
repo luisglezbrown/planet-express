f75e129006a865974ac16238a78527236647245c
luisglezbrown <lfernanglez@gmail.com>
2021-09-04 20:08:28 +0200

    Corrected text

--- src/components/OfferCountdown.js
+++ src/components/OfferCountdown.js
@@ -35,7 +35,7 @@ export default function OfferCountdown () {
             { minutesRemaining === 0 && secondsRemaining === 0
                 ? null
                 : <section className="offer">
-                    <h3> La oferta expira en {minutesRemaining}:{secondsRemaining < 10 ?  `0${secondsRemaining}` : secondsRemaining}</h3> 
+                    <h3> La oferta expira en {minutesRemaining}h. {secondsRemaining < 10 ?  `0${secondsRemaining}` : secondsRemaining}m.</h3> 
                 </section>
             }
         </>
