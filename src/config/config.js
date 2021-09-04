e0e49836e6d8cd8bab374ca0728cecb449bb0278
luisglezbrown <lfernanglez@gmail.com>
2021-09-04 20:08:48 +0200

    Added URLs

--- src/config/config.js
+++ src/config/config.js
@@ -6,9 +6,19 @@ const PRICE_PER_KG = 0.95;
 const COUNTDOWN_INITIAL_MINUTES = 3;
 const COUNTDOWN_INITIAL_SECONDS = 0;
 
+// Images and links URLs
+const LOGO = "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1f/Futurama_Planet_Express.svg/1110px-Futurama_Planet_Express.svg.png";
+const OFFICE_URL = "https://live.staticflickr.com/1007/1332747378_6c15e7b835_b.jpg";
+const FLEET_URL = "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/65b1d26b-68cc-4254-9d9d-61b49c7708e9/dkr8md-ff3afa18-8387-44f7-97be-1765f209eb01.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzY1YjFkMjZiLTY4Y2MtNDI1NC05ZDlkLTYxYjQ5Yzc3MDhlOVwvZGtyOG1kLWZmM2FmYTE4LTgzODctNDRmNy05N2JlLTE3NjVmMjA5ZWIwMS5qcGcifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.s7yxFWCCiadtWlUvaBYscpcGmlpJTYXqq2x2eD76m2g";
+const API_URL = "http://futuramaapi.herokuapp.com/api/v2/characters";
+
 export {
     PACKAGE_FEE,
     PRICE_PER_KG,
     COUNTDOWN_INITIAL_MINUTES, 
-    COUNTDOWN_INITIAL_SECONDS    
+    COUNTDOWN_INITIAL_SECONDS,
+    LOGO,
+    OFFICE_URL, 
+    FLEET_URL,
+    API_URL     
 };
