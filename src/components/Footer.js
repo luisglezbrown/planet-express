cdafacd8d1f85b6c4bb8b91155fee020d840cfe5
luisglezbrown <lfernanglez@gmail.com>
2021-09-04 20:07:17 +0200

    Created Footer Component

---
+++ src/components/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <footer>
+            <p>Hecho con 🤍 por <a href="https://github.com/luisglezbrown" target="_blank" rel="noreferrer">Luis González</a> || Málaga [ES]</p>
+        </footer>
+    )
+}
