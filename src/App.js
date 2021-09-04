cc832fb45798ea60c0410e07bb0f8d5b20dfbff9
luisglezbrown <lfernanglez@gmail.com>
2021-09-04 20:06:44 +0200

    Added all components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import Newsletter from './components/Newsletter';
+import Header from './components/Header';
 import Calculator from './components/Calculator';
+import OfferCountdown from './components/OfferCountdown';
+import TheCompany from './components/TheCompany';
+import Newsletter from './components/Newsletter';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div>
-      <Calculator />
-      <Newsletter />
-    </div>
+    <>
+      <Header />
+      <main>
+        <Calculator />
+        <OfferCountdown />
+        <TheCompany />
+        <Newsletter />
+      </main>
+      <Footer/>
+    </>
   );
 }
 
